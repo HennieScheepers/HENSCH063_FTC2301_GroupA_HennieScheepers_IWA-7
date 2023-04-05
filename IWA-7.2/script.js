913ae483637acdaee32b11bfa9045135786afd70
Hennie Scheepers <hennie.104@hotmail.com>
2023-04-05 12:59:59 +0200

    Completed challenge 2

---
+++ IWA-7.2/script.js
@@ -0,0 +1,8 @@
+const nickname= null;
+const firstname = null;
+const greeting = nickname || firstname ? `Good morning, ${nickname || firstname}!` : 'Good morning!'
+
+console.log(greeting)
+
+/*The code did not work because there was no conditional to check if the firstname or nickname was present.
+* We need to add a conditional to check if they have values otherwise it should return a generic greeting */
