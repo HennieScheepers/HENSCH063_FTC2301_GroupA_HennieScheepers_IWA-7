1d2188df5a5032f9f26a6d1551acecd115b759c7
Hennie Scheepers <hennie.104@hotmail.com>
2023-04-05 13:00:56 +0200

    Changed comments

--- IWA-7.2/script.js
+++ IWA-7.2/script.js
@@ -5,4 +5,5 @@ const greeting = nickname || firstname ? `Good morning, ${nickname || firstname}
 console.log(greeting)
 
 /*The code did not work because there was no conditional to check if the firstname or nickname was present.
-* We need to add a conditional to check if they have values otherwise it should return a generic greeting */
+* We need to add a conditional to check if they have values otherwise it should return a generic greeting.
+* We also need to use backticks when we interpolate*/
