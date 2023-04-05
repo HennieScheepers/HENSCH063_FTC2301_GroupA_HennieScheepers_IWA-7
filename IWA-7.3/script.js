d5f58efa138aa6863e7771620ad169fbe7a6df3f
Hennie Scheepers <hennie.104@hotmail.com>
2023-04-05 14:53:11 +0200

    Added the .toFixed after the parsing

--- IWA-7.3/script.js
+++ IWA-7.3/script.js
@@ -13,7 +13,7 @@ const owed = `R${parseFloat((leoBalance * -1) + (sarahBalance * -1)).toFixed(2)}
 
 const leo = `\n${leoName} ${leoSurname.substring(0,7)} (Owed: R${(parseFloat(leoBalance) * -1).toFixed(2)})`
 
-const sarah = `\n${sarahName.substring(0,5)}  ${sarahSurname} (Owed: R${parseFloat(sarahBalance).toFixed(2) * -1})\n\n`
+const sarah = `\n${sarahName.substring(0,5)}  ${sarahSurname} (Owed: R${(parseFloat(sarahBalance) * -1).toFixed(2)})\n\n`
 
 const total = `\n  Total amount owed: ${owed.substring(0, 3)} ${owed.substring(3, owed.length)}`
 
