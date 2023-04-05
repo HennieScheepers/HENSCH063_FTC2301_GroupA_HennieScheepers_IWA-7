7e2cf52dea1044885c32b2a80d80f0fde569373e
Hennie Scheepers <hennie.104@hotmail.com>
2023-04-05 15:18:51 +0200

    Cganged spacing between elements

--- IWA-7.3/script.js
+++ IWA-7.3/script.js
@@ -11,9 +11,9 @@ const divider = '----------------------------------'
 // Only change below this line
 const owed = `R${parseFloat((leoBalance * -1) + (sarahBalance * -1)).toFixed(2)} \n`
 
-const leo = `\n${leoName} ${leoSurname.substring(0,7)} (Owed: R${(parseFloat(leoBalance) * -1).toFixed(2)})`
+const leo = `\n${leoName} ${leoSurname.substring(0,7)} (Owed: R ${(parseFloat(leoBalance) * -1).toFixed(2)})`
 
-const sarah = `\n${sarahName.substring(0,5)}  ${sarahSurname} (Owed: R${(parseFloat(sarahBalance) * -1).toFixed(2)})\n\n`
+const sarah = `\n${sarahName.substring(0,5)}  ${sarahSurname} (Owed: R ${(parseFloat(sarahBalance) * -1).toFixed(2)})\n\n`
 
 const total = `\n  Total amount owed: ${owed.substring(0, 3)} ${owed.substring(3, owed.length)}`
 
