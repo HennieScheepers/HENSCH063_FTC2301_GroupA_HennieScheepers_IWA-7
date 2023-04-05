e98592268dccbebc2826ce8c2895426bf0100e64
Hennie Scheepers <hennie.104@hotmail.com>
2023-04-05 12:46:03 +0200

    Completed challenge 1

---
+++ IWA-7.1/script.js
@@ -0,0 +1,5 @@
+const value = 3
+console.log(value + 4 + value)
+
+/*The problem with this code is that as soon as values get added together and there is a string, 
+* everything ofter the string gets coerced into a string and therefore the values get concatenated*/
